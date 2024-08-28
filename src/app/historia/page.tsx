f4189071e0bce4b7809cbea4785fbc0f4b7c4d60
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2024-08-28 19:27:02 -0300

    Update page.tsx

--- src/app/historia/page.tsx
+++ src/app/historia/page.tsx
@@ -1,20 +1,38 @@
 import CardImgText from "@/components/cardImgText";
 import CardText from "@/components/cardText";
 import SectionCard from "@/components/sectionCard";
+import { Card, CardContent, CardDescription } from "@/components/ui/card";
 
-export default function Historia(){
+export default function Historia() {
     return <>
         <main className="flex flex-col gap-5">
             <div className="flex justify-center py-8">
                 <h1 className="text-4xl font-bold text-justify">
-                    Visitação com explicação sobre a cultura e valorização do protagonismo negro
+                    A história do Morro da África e imigração de negros e pardos em Jaraguá do Sul.
                 </h1>
             </div>
+
             <CardImgText
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCo2KEbK1D_mfuaRtULvTNKq-r-bRyVAfa_A&s"
                 acervo="Acervo Pessoal"
-                texto="O projeto veio demonstrar uma cultura que sofreu com ataques a sua história, tentando apagar suas raízes. O canto do hino veio como maneira para demonstrar um pouco dessa cultura tão sofrida, a fim de reviver e apresentar as formas de luta e resistência africana contra o racismo estrutural preso à sociedade.">
+                texto="Em 1876 Emílio Carlos Jourdan, engenheiro belga, chegou à Jaraguá do Sul com 35 escravos, 54 negros e 6 brancos, ambos livres, a pedido da Princesa Isabel para colonizar as terras com o plano do Empreendimento Jaraguá, que seria formado por engenhos, usinas de açúcar, serrarias, olárias e ferrarias.
+                Porém, por volta de 1984 o engenho faliu, e Jourdan voltou para o Rio de Janeiro com seus escravos, sem pagar os 60 trabalhadores que ele levou. Em 1895 essas terras voltaram a ser públicas e Jourdan comprou 10 mil hectares, fundando a Colônia Jaraguá, logo depois expulsou os então habitantes, que se locomoveram para o Morro da Boa Vista, também conhecido como Morro da África ou Morro da Antena.
+                Um ano após comprar as terras, Jourdan as vendeu, e em 1907 alguns nomes conhecidos como  Ângelo Piazeira, Cesar Pereira de Souza e Francisco Tavares da Cunha Mello Sobrinho compraram a Colônia Jaraguá (alemães)."
+            >
             </CardImgText>
+
+            <div className="flex flex-col gap-4 justify-center w-full">
+                <Card className="flex flex-col gap-4 p-6">
+                    <img className="w-full object-cover h-auto border-0 rounded-sm" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCo2KEbK1D_mfuaRtULvTNKq-r-bRyVAfa_A&s" alt="" />
+                    <CardDescription>Acervo Pessoal</CardDescription>
+                    <CardContent className="p-0">
+                        <CardDescription className="text-justify text-lg">Características geográficas do Morro da Boa Vista</CardDescription>
+                        <CardDescription className="text-justify text-lg">Altitude - 824m</CardDescription>
+                        <CardDescription className="text-justify text-lg">Clima - Subtropical, com temperaturas médias de 21°C</CardDescription>
+                        <CardDescription className="text-justify text-lg">Vegetação - Mata Atlântica.</CardDescription>
+                    </CardContent>
+                </Card>
+            </div>
 
             {/* <div className="flex flex-col gap-8">
                 <SectionCard tituloSection={brincadeiras.tituloSection} cards={brincadeiras.cards} />
