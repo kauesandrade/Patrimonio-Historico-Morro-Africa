c493ac88434816079a4c1f67db3a4c99dffae5fc
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2024-09-01 15:23:17 -0300

    Update cardImgText.tsx

--- src/components/cardImgText.tsx
+++ src/components/cardImgText.tsx
@@ -1,18 +1,16 @@
 'use client';
 
-import { useRouter } from "next/navigation";
 import { Card, CardDescription, CardTitle } from "./ui/card";
+import { useRouter } from "next/navigation";
 
 export default function cardImgText(props: any) {
-
-    const router = useRouter();
 
     return <>
         <div className="flex flex-col gap-4 justify-center w-full">
             <Card className="flex flex-col gap-4 p-6">
                 <img className="w-full object-cover h-auto border-0 rounded-sm max-h-96" src={props.src} alt="" />
 
-                <CardDescription onClick={() => props.acervoUrl != "" ? router.push(props.acervoUrl) : null}>
+                <CardDescription onClick={() => props.acervoUrl != "" ? useRouter().push(props.acervoUrl) : null}>
                     <a className={props.acervoUrl != "" ? "" : "cursor-pointer text-sky-600 underline"}>{props.acervo}</a>
                 </CardDescription>
 
